cd6fa13c7c2d7c4566e210137b8db045c514ce28
kitae <iampkt22@gmail.com>
2023-09-02 23:59:20 +0900

    Add Week-02

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,71 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 20f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+
+    public float gravity = -10f;
+
+    Vector3 velocity;
+
+
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
+
+    public float jumpHeight = 3f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        print(direction.magnitude);
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float taretAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, taretAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, taretAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
+
+
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            if(isGrounded && velocity.y <0)
+            {
+                velocity.y = -2f;
+            }
+
+            if(Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+        }
+    }
+}
